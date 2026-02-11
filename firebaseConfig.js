71c6aeadee400c404509f3171dec65fdc2214bf6
OffshoreSea <krisarvesen@gmail.com>
2026-02-11 11:20:45 +0100

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyC_tUc4FPoGjTjCvu7ucJG-S8PB7DaU3W8",
+  authDomain: "pioneer-welfare.firebaseapp.com",
+  projectId: "pioneer-welfare",
+  storageBucket: "pioneer-welfare.firebasestorage.app",
+  messagingSenderId: "406179084430",
+  appId: "1:406179084430:web:cda1ee722faac2942231df",
+  measurementId: "G-6KDZTDTZD7"
+};
